3d38fecb721f61e633ce5a3f74314b1e0796e814
Jonathon Love <jon@thon.cc>
2016-05-12 14:28:26 +1000

    General updates

--- R/descriptives.b.R
+++ R/descriptives.b.R
@@ -1,9 +1,9 @@
 
-DescriptivesClass <- R6Class("DescriptivesClass",
+DescriptivesClass <- R6::R6Class("DescriptivesClass",
     inherit=silkycore::Analysis,
     private=list(
         .run=function() {
-        
+            
             dataset <- self$options$dataset()
             
             desc <- self$results$get("descriptives")
@@ -18,51 +18,36 @@ DescriptivesClass <- R6Class("DescriptivesClass",
                     
                     numColumn <- as.numeric(column)
                     
-                    desc$setCell(i, "mean", mean(numColumn))
-                    desc$setCell(i, "median", median(numColumn))
+                    desc$setCell(rowNo=i, "mean", mean(numColumn))
+                    desc$setCell(rowNo=i, "median", median(numColumn))
                     
                     mode <- as.numeric(names(table(numColumn)[table(numColumn)==max(table(numColumn))]))
-                    desc$setCell(i, "mode", mode[1])
+                    desc$setCell(rowNo=i, "mode", mode[1])
                     
-                    desc$setCell(i, "sum", sum(numColumn))
-                    desc$setCell(i, "sd", sd(numColumn))
-                    desc$setCell(i, "variance", var(numColumn))
-                    desc$setCell(i, "range", max(numColumn)-min(numColumn))
-                    desc$setCell(i, "min", min(numColumn))
-                    desc$setCell(i, "max", max(numColumn))
-                    desc$setCell(i, "se", sqrt(var(numColumn)/length(numColumn)))
+                    desc$setCell(rowNo=i, "sum", sum(numColumn))
+                    desc$setCell(rowNo=i, "sd", sd(numColumn))
+                    desc$setCell(rowNo=i, "variance", var(numColumn))
+                    desc$setCell(rowNo=i, "range", max(numColumn)-min(numColumn))
+                    desc$setCell(rowNo=i, "min", min(numColumn))
+                    desc$setCell(rowNo=i, "max", max(numColumn))
+                    desc$setCell(rowNo=i, "se", sqrt(var(numColumn)/length(numColumn)))
                     
                     deviation <- numColumn-mean(numColumn)
-                    desc$setCell(i, "skew", sum(deviation^3)/(length(numColumn)*sd(numColumn)^3))
-                    desc$setCell(i, "kurt", sum(deviation^4)/(length(numColumn)*var(numColumn)^2))
+                    desc$setCell(rowNo=i, "skew", sum(deviation^3)/(length(numColumn)*sd(numColumn)^3))
+                    desc$setCell(rowNo=i, "kurt", sum(deviation^4)/(length(numColumn)*var(numColumn)^2))
                     
-                    desc$setCell(i, "quart1", quantile(numColumn, c(.25)))
-                    desc$setCell(i, "quart2", quantile(numColumn, c(.5)))
-                    desc$setCell(i, "quart3", quantile(numColumn, c(.75)))
+                    desc$setCell(rowNo=i, "quart1", quantile(numColumn, c(.25)))
+                    desc$setCell(rowNo=i, "quart2", quantile(numColumn, c(.5)))
+                    desc$setCell(rowNo=i, "quart3", quantile(numColumn, c(.75)))
                     
                 } else {
                     
                     mode <- NULL
                     
-                    desc$setCell(i, "mean", "")
-                    desc$setCell(i, "median", "")
-                    
-                    # mode <- names(table(column)[table(column)==max(table(column))])
-                    # desc$setCell(i, "mode", mode[1])
-                    desc$setCell(i, "mode", "")
-                    
-                    desc$setCell(i, "sum", "")
-                    desc$setCell(i, "sd", "")
-                    desc$setCell(i, "variance", "")
-                    desc$setCell(i, "range", "")
-                    desc$setCell(i, "min", "")
-                    desc$setCell(i, "max", "")
-                    desc$setCell(i, "se", "")
-                    desc$setCell(i, "skew", "")
-                    desc$setCell(i, "kurt", "")
-                    desc$setCell(i, "quart1", "")
-                    desc$setCell(i, "quart2", "")
-                    desc$setCell(i, "quart3", "")
+                    desc$setRow(rowNo=i, values=list(
+                        mean=NA, median=NA, mode=NA, sum=NA, sd=NA, variance=NA,
+                        range=NA, min=NA, max=NA, se=NA, skew=NA, kurt=NA,
+                        quart1=NA, quart2=NA, quart3=NA))
                 }
                 
                 cumCount <- 0
@@ -74,15 +59,19 @@ DescriptivesClass <- R6Class("DescriptivesClass",
                     count <- sum(column == levels[j])
                     cumCount <- cumCount + count
                     
-                    freqTable$setCell(j, "counts", count)
-                    freqTable$setCell(j, "percentage", 100*count/length(column))
-                    freqTable$setCell(j, "cumpercentage", 100*cumCount/length(column))
+                    freqTable$setCell(rowNo=j, "counts", count)
+                    freqTable$setCell(rowNo=j, "percentage", 100*count/length(column))
+                    freqTable$setCell(rowNo=j, "cumpercentage", 100*cumCount/length(column))
                 }
                 
                 if (length(mode) > 1)
-                    desc$addFootnote(i, "mode", "More than one mode exists, only the first is reported")
+                    desc$addFootnote(rowNo=i, "mode", "More than one mode exists, only the first is reported")
                 
             }
+        },
+        .plotCorr=function(image, ...) {
+            print("rendering")
+            hist(rnorm(200))
         }
     )
 )
