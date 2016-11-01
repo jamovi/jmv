4f1be68f2331322ecb770dfbf423f1c4c2c8389c
raviselker <selker.ravi@gmail.com>
2016-11-01 18:43:29 +0100

    Paired Cont. Table: modified analysis for new UI

--- R/conttablespaired.b.R
+++ R/conttablespaired.b.R
@@ -4,201 +4,248 @@ ContTablesPairedClass <- R6::R6Class(
     inherit = ContTablesPairedBase,
     private = list(
         .run = function() {
+            
+            rowVarName <- self$options$rows
+            colVarName <- self$options$cols
+    
+            if (is.null(rowVarName) || is.null(colVarName))
+                return()
+    
+            if (nlevels(self$data[[rowVarName]]) < 2)
+                jmvcore::reject("Row variable '{}' contains fewer than 2 levels", code='', rowVarName)
+            if (nlevels(self$data[[colVarName]]) < 2)
+                jmvcore::reject("Column variable '{}' contains fewer than 2 levels", code='', colVarName)
+            
+            countName <- self$options$counts
+            if ( ! is.null(countName)) {
+                countCol <- jmvcore::toNumeric(self$data[[countName]])
+                if (any(countCol < 0, na.rm=TRUE))
+                    jmvcore::reject('Counts may not be negative')
+                if (any(is.infinite(countCol)))
+                    jmvcore::reject('Counts may not be infinite')
+            }
+            
+            freqs <- self$results$get('freqs')
+            test  <- self$results$get('test')
+            
+            data <- jmvcore::select(self$data, c(rowVarName, colVarName, countName))
+            data <- jmvcore::naOmit(data)
+            
+            rowVar <- data[[rowVarName]]
+            colVar <- data[[colVarName]]
+            
+            if (! is.null(countName))
+                result <- stats::xtabs(countCol ~ rowVar + colVar)
+            else
+                result <- base::table(rowVar, colVar)
+            
+            colTotals <- apply(result, 2, base::sum)
+            freqRowNo <- 1
+            
+            for (rowNo in seq_len(nrow(result))) {
 
-            tables <- self$results$get('tables')
-            tests  <- self$results$get('tests')
+                counts <- result[rowNo,]
 
-            for (i in seq_along(self$options$pairs)) {
+                if (length(counts) > 0) {
+                    rowTotal <- sum(counts)
+                    pcRow <- counts / rowTotal
+                    pcCol <- counts / colTotals
 
-                pair <- self$options$pairs[[i]]
-                if (is.null(pair[[1]]) || is.null(pair[[2]]))
-                    next()
-
-                table <- tables$get(pair)
-                item1 <- pair[[1]]
-                item2 <- pair[[2]]
-
-                data <- jmvcore::select(self$data, pair)
-                data <- jmvcore::naOmit(data)
-
-                data1 <- data[[1]]
-                if (ncol(data) == 1)  # variable paired with itself
-                    data2 <- data[[1]]
-                else
-                    data2 <- data[[2]]
-
-                if (self$options$areCounts) {
-
-                    if (jmvcore::canBeNumeric(data1))
-                        data1 <- jmvcore::toNumeric(data1)
-                    else
-                        data1 <- suppressWarnings(as.numeric(as.character(data1)))
-
-                    if (jmvcore::canBeNumeric(data2))
-                        data2 <- jmvcore::toNumeric(data2)
-                    else
-                        data2 <- suppressWarnings(as.numeric(as.character(data2)))
-
-                    data  <- cbind(data1, data2)
-
-                    result <- as.table(as.matrix(data))
-
-                } else {
-                    result <- base::table(data1, data2)
+                    names(counts) <- paste0(seq_len(length(counts)), '[count]')
+                    names(pcRow)  <- paste0(seq_len(length(counts)), '[pcRow]')
+                    names(pcCol)  <- paste0(seq_len(length(counts)), '[pcCol]')
+                    names(rowTotal)  <- '.total[count]'
+                    
+                    freqs$setRow(rowNo=rowNo, values=c(counts, pcRow, pcCol, rowTotal))
+                    freqRowNo <- freqRowNo + 1
                 }
+            }
+            
+            nCols <- length(colTotals)
+            
+            rowTotal <- base::sum(colTotals)
+            values <- as.list(colTotals)
+            names(values) <- paste0(1:nCols, '[count]')
+            values[['.total[count]']] <- rowTotal
 
-                colTotals <- apply(result, 2, base::sum)
+            pcRow <- colTotals / rowTotal
+            pcRow <- as.list(pcRow)
+            names(pcRow) <- paste0(1:nCols, '[pcRow]')
 
-                for (rowNo in seq_len(nrow(result))) {
+            pcCol <- rep(1, nCols)
+            pcCol <- as.list(pcCol)
+            names(pcCol) <- paste0(1:nCols, '[pcCol]')
+            
+            names(rowTotal)  <- '.total[count]'
 
-                    counts <- result[rowNo,]
+            values <- c(values, pcRow, pcCol, rowTotal)
 
-                    if (length(counts) > 0) {
-                        rowTotal <- sum(counts)
-                        pcRow <- counts / rowTotal
-                        pcCol <- counts / colTotals
-
-                        names(counts) <- paste0(seq_len(length(counts)), '[count]')
-                        names(pcRow)  <- paste0(seq_len(length(counts)), '[pcRow]')
-                        names(pcCol)  <- paste0(seq_len(length(counts)), '[pcCol]')
-                        table$setRow(rowNo=rowNo, values=c(counts, pcRow, pcCol))
-                    }
-                }
-
-                wocor <- try(mcnemar.test(result, correct=FALSE), silent=TRUE)
-                wcor  <- try(mcnemar.test(result, correct=TRUE), silent=TRUE)
-
-                values <- list()
-
-                if (base::inherits(wocor, 'try-error') || is.na(wocor$statistic)) {
-                    values[['value[mcn]']] <- NaN
-                    values[['df[mcn]']] <- ''
-                    values[['p[mcn]']]  <- ''
-                } else {
-                    values[['value[mcn]']] <- wocor$statistic
-                    values[['df[mcn]']] <- wocor$parameter
-                    values[['p[mcn]']]  <- wocor$p.value
-                }
-
-                if (base::inherits(wcor, 'try-error') || is.na(wcor$statistic)) {
-                    values[['value[cor]']] <- NaN
-                    values[['df[cor]']] <- ''
-                    values[['p[cor]']]  <- ''
-                } else {
-                    values[['value[cor]']] <- wcor$statistic
-                    values[['df[cor]']] <- wcor$parameter
-                    values[['p[cor]']]  <- wcor$p.value
-                }
-
-                tests$setRow(rowNo=i, values=values)
-
-                if (base::inherits(wocor, 'try-error')) {
-                    error <- jmvcore::extractErrorMessage(wocor)
-                    if (error == "'x' must be square with at least two rows and columns")
-                        error <- 'McNemar requires a 2x2 table'
-                    else if (error == "all entries of 'x' must be nonnegative and finite")
-                        error <- 'Counts must be non-negative and finite'
-                    tests$addFootnote(rowNo=i, 'value[mcn]', error)
-                }
-
-                if (base::inherits(wcor, 'try-error')) {
-                    error <- jmvcore::extractErrorMessage(wcor)
-                    if (error == "'x' must be square with at least two rows and columns")
-                        error <- 'McNemar requires a 2x2 table'
-                    else if (error == "all entries of 'x' must be nonnegative and finite")
-                        error <- 'Counts must be non-negative and finite'
-                    tests$addFootnote(rowNo=i, 'value[cor]', error)
-                }
+            freqs$setRow(freqRowNo, values=values)
+            
+            wocor <- try(stats::mcnemar.test(result, correct=FALSE), silent=TRUE)
+            wcor  <- try(stats::mcnemar.test(result, correct=TRUE), silent=TRUE)
+            
+            values <- list()
+            
+            if (base::inherits(wocor, 'try-error') || is.na(wocor$statistic)) {
+                values[['value[mcn]']] <- NaN
+                values[['df[mcn]']] <- ''
+                values[['p[mcn]']]  <- ''
+            } else {
+                values[['value[mcn]']] <- wocor$statistic
+                values[['df[mcn]']] <- wocor$parameter
+                values[['p[mcn]']]  <- wocor$p.value
+            }
+            
+            if (base::inherits(wcor, 'try-error') || is.na(wcor$statistic)) {
+                values[['value[cor]']] <- NaN
+                values[['df[cor]']] <- ''
+                values[['p[cor]']]  <- ''
+            } else {
+                values[['value[cor]']] <- wcor$statistic
+                values[['df[cor]']] <- wcor$parameter
+                values[['p[cor]']]  <- wcor$p.value
+            }
+            
+            test$setRow(rowNo=1, values=values)
+            
+            if (base::inherits(wocor, 'try-error')) {
+                error <- jmvcore::extractErrorMessage(wocor)
+                if (error == "'x' must be square with at least two rows and columns")
+                    error <- 'McNemar requires a 2x2 table'
+                else if (error == "all entries of 'x' must be nonnegative and finite")
+                    error <- 'Counts must be non-negative and finite'
+                test$addFootnote(rowNo=1, 'value[mcn]', error)
+            }
+            
+            if (base::inherits(wcor, 'try-error')) {
+                error <- jmvcore::extractErrorMessage(wcor)
+                if (error == "'x' must be square with at least two rows and columns")
+                    error <- 'McNemar requires a 2x2 table'
+                else if (error == "all entries of 'x' must be nonnegative and finite")
+                    error <- 'Counts must be non-negative and finite'
+                test$addFootnote(rowNo=1, 'value[cor]', error)
             }
         },
         .init = function() {
 
-            tables <- self$results$get('tables')
-            pairs <- self$options$pairs
+            freqs <- self$results$get('freqs')
+            rowVarName <- self$options$rows
+            colVarName <- self$options$cols
 
-            if (length(pairs) == 0) {
-                pair <- list(NULL, NULL)
-                pairs <- list(pair)
-                tables$addItem(pair)
+            # add the row column, containing the row variable
+            # fill in dots, if no row variable specified
+
+            if ( ! is.null(rowVarName))
+                title <- rowVarName
+            else
+                title <- '.'
+    
+            freqs$addColumn(
+                name=title,
+                title=title,
+                type='text')
+            
+            # add the column columns (from the column variable)
+            # fill in dots, if no column variable specified
+    
+            if ( ! is.null(colVarName)) {
+                superTitle <- colVarName
+                levels <- base::levels(self$data[[colVarName]])
             }
-
-            for (pair in pairs) {
-                table <- tables$get(pair)
-                item1 <- pair[[1]]
-                item2 <- pair[[2]]
-
-                if (is.null(item1)) {
-                    item1 <- '…'
-                    item1levels <- c('1', '2')
-                } else if (self$options$areCounts) {
-                    item1levels <- c('1', '2')
-                } else {
-                    item1levels <- base::levels(self$data[[item1]])
-                    if (length(item1levels) == 0)
-                        item1levels <- c('1', '2')
-                }
-
-                if (is.null(item2)) {
-                    item2 <- '…'
-                    item2levels <- c('1', '2')
-                } else if (self$options$areCounts) {
-                    item2levels <- c('1', '2')
-                } else {
-                    item2levels <- base::levels(self$data[[item2]])
-                    if (length(item2levels) == 0)
-                        item2levels <- c('1', '2')
-                }
-
-                table$setTitle(paste(item1, '-', item2))
-
-                table$addColumn(
-                    name='row',
-                    title=item1,
-                    type='text')
-
-                subNames  <- c('[count]', '[pcRow]', '[pcCol]')
-                subTitles <- c('Count', '% within row', '% within column')
-                visible   <- c('(obs)', '(pcRow)', '(pcCol)')
-                types     <- c('integer', 'number', 'number')
-                formats   <- c('', 'pc', 'pc')
-
-                # iterate over the sub rows
-
+            else {
+                superTitle <- '.'
+                levels <- c('.', '.')
+            }
+            
+            hasSubRows <- sum(self$options$pcRow,
+                              self$options$pcCol) > 0
+            
+            subNames  <- c('[count]', '[pcRow]', '[pcCol]')
+            subTitles <- c('Count', '% within row', '% within column')
+            visible   <- c('(obs)', '(pcRow)', '(pcCol)')
+            types     <- c('integer', 'number', 'number')
+            formats   <- c('', 'pc', 'pc')
+            
+            # iterate over the sub rows
+            
+            for (j in seq_along(subNames)) {
+                subName <- subNames[[j]]
+                if (j == 1)
+                    v <- '(obs && (pcRow || pcCol))'
+                else
+                    v <- visible[j]
+    
+                freqs$addColumn(
+                    name=paste0('type', subName),
+                    title='',
+                    type='text',
+                    visible=v)
+            }
+            
+            for (i in seq_along(levels)) {
+                level <- levels[[i]]
+    
                 for (j in seq_along(subNames)) {
                     subName <- subNames[[j]]
-                    if (j == 1)
-                        v <- '(pcRow || pcCol)'
-                    else
-                        v <- visible[j]
-
-                    table$addColumn(
-                        name=paste0('type', subName),
-                        title='',
-                        type='text',
-                        visible=v)
+                    freqs$addColumn(
+                        name=paste0(i, subName),
+                        title=level,
+                        superTitle=superTitle,
+                        type=types[j],
+                        format=formats[j],
+                        visible=visible[j])
                 }
-
-                for (i in seq_along(item2levels)) {
-                    level <- item2levels[[i]]
-
-                    for (j in seq_along(subNames)) {
-                        subName <- subNames[[j]]
-                        table$addColumn(
-                            name=paste0(i, subName),
-                            title=level,
-                            superTitle=item2,
-                            type=types[j],
-                            format=formats[j],
-                            visible=visible[j])
-                    }
-                }
-
-                values <- list()
-                for (i in seq_along(subNames))
-                    values[[paste0('type', subNames[i])]] <- subTitles[i]
-
-                for (level in item1levels)
-                    table$addRow(rowKey=level, values=c(values, list(row=level)))
             }
+            
+            # add the Total column
+
+            freqs$addColumn(
+                name='.total[count]',
+                title='Total',
+                type='integer')
+            
+            # populate the first column with levels of the row variable
+    
+            values <- list()
+            for (i in seq_along(subNames))
+                values[[paste0('type', subNames[i])]] <- subTitles[i]
+    
+            rows <- private$.grid(incRows=TRUE)
+            
+            for (i in seq_len(nrow(rows))) {
+                for (name in dimnames(rows)[[2]]) {
+                    value <- as.character(rows[i, name])
+                    if (value == '.total')
+                        value <- 'Total'
+                    values[[name]] <- value
+                }
+                key <- paste0(rows[i,], collapse='`')
+                freqs$addRow(rowKey=key, values=values)
+    
+                if (hasSubRows == FALSE && rows[i, ncol(rows)] == '.total')
+                    freqs$addFormat(rowNo=i, 1, Cell.BEGIN_END_GROUP)
+            }
+ 
+            test <- self$results$get('test')
+            test$addRow(rowKey=1, values=list())
+
+        },
+        .grid=function(incRows=FALSE) {
+    
+            rowVarName <- self$options$get('rows')
+            
+            expand <- list()
+    
+            if (incRows) {
+                if (is.null(rowVarName))
+                    expand[['.']] <- c('.', '. ', 'Total')
+                else
+                    expand[[rowVarName]] <- c(base::levels(self$data[[rowVarName]]), '.total')
+            }
+    
+            rows <- rev(expand.grid(expand))
+            
+            rows
         })
 )
