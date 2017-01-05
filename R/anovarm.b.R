dbb8ef0d923029388ddc933849aa7be4220189f8
raviselker <selker.ravi@gmail.com>
2017-01-05 14:36:39 +0100

    RM ANOVA: basic implementation

--- R/anovarm.b.R
+++ R/anovarm.b.R
@@ -7,14 +7,19 @@ AnovaRMClass <- R6::R6Class(
         .init=function() {
             
             rmTable <- self$results$get('rmTable')
+            rmTable$setNote("Note", jmvcore::format("Type {} Sums of Squares", self$options$ss))
             
             rm <- private$.rmTerms()
             rmTerms <- rm$terms
             rmSpacing <- rm$spacing
             
             if (length(rmTerms) > 0) {
-                for (term in rmTerms)
-                    rmTable$addRow(rowKey=term, list(name=stringifyTerm(term)))
+                for (i in seq_along(rmTerms)) {
+                    if (rmTerms[i] == "Residual")
+                        rmTable$addRow(rowKey=unlist(c(rmTerms[i-1],".RES")), list(name=stringifyTerm(rmTerms[i])))
+                    else
+                        rmTable$addRow(rowKey=unlist(rmTerms[i]), list(name=stringifyTerm(rmTerms[i])))
+                }
             } else {
                 rmTable$addRow(rowKey='.', list(name='.'))
                 rmTable$addRow(rowKey='', list(name='Residual'))
@@ -32,11 +37,12 @@ AnovaRMClass <- R6::R6Class(
             }
 
             bsTable <- self$results$get('bsTable')
+            bsTable$setNote("Note", jmvcore::format("Type {} Sums of Squares", self$options$ss))
             
             bsTerms <- private$.bsTerms()
             if (length(bsTerms) > 0) {
                 for (term in bsTerms)
-                    bsTable$addRow(rowKey=term, list(name=stringifyTerm(term)))
+                    bsTable$addRow(rowKey=unlist(term), list(name=stringifyTerm(term)))
             } else {
                 bsTable$addRow(rowKey='', list(name='Residual'))
             }
@@ -57,14 +63,26 @@ AnovaRMClass <- R6::R6Class(
                 
                 data <- private$.wideToLong()
                 modelFormula <- private$.modelFormula()
-                model <- afex::aov_car(modelFormula, data)
                 
-                print(summary(model))
+                print(head(data))
+                print(modelFormula)
+                
+                suppressWarnings({
+                    
+                    result <- try(afex::aov_car(modelFormula, data, type=self$options$ss), silent=TRUE)
+                    
+                }) # suppressWarnings
+                
+                if (isError(result)) {
+                    print(extractErrorMessage(result))
+                } else {
+                    private$.populateTables(result)
+                }
             }
         },
         .rmTerms=function() {
             
-            if (is.null(self$options$rmTerms) || is.null(self$options$bsTerms)) { # if no specific model is specified
+            if (length(self$options$rmTerms) == 0) { # if no specific model is specified
                 
                 rmFactors <- self$options$rm
                 bsFactors <- self$options$bs
@@ -129,7 +147,7 @@ AnovaRMClass <- R6::R6Class(
         },
         .bsTerms=function() {
             
-            if (is.null(self$options$rmTerms) || is.null(self$options$bsTerms)) { # if no specific model is specified
+            if (length(self$options$bsTerms) == 0) { # if no specific model is specified
                 
                 bsFactors <- self$options$bs
                 covariates <- self$options$cov
@@ -201,9 +219,9 @@ AnovaRMClass <- R6::R6Class(
             for (var in bsVars)
                 data[[var]] <- self$data[[var]]
             
-            data <- jmvcore::naOmit(data)
             attr(data, 'row.names') <- seq_len(length(data[[1]]))
             attr(data, 'class') <- 'data.frame'
+            data <- jmvcore::naOmit(data)
             
             data <- cbind(data, "subject" = factor(1:nrow(data)))
             
@@ -220,9 +238,9 @@ AnovaRMClass <- R6::R6Class(
             }
             data_long[["variable"]] <- NULL
             
-            data_long <- jmvcore::naOmit(data_long)
             attr(data_long, 'row.names') <- seq_len(length(data_long[[1]]))
             attr(data_long, 'class') <- 'data.frame'
+            data_long <- jmvcore::naOmit(data_long)
 
             return(data_long)
         },
@@ -234,8 +252,8 @@ AnovaRMClass <- R6::R6Class(
                 
             } else {
                 
-                bsTerms <- self$options$bsTerms
-                rmTerms <- self$options$rmTerms
+                bsTerms <- lapply(self$options$bsTerms, function(x) gsub('`', '\\`', x, fixed=TRUE))
+                rmTerms <- lapply(self$options$rmTerms, function(x) gsub('`', '\\`', x, fixed=TRUE))
                 
                 bsItems <- list()
                 for (i in seq_along(bsTerms))
@@ -265,6 +283,81 @@ AnovaRMClass <- R6::R6Class(
                 return(formula)
                 
             }
+        },
+        .populateTables=function(result) {
+            
+            rmTable <- self$results$get('rmTable')
+            bsTable <- self$results$get('bsTable')
+            
+            rmTerms <- private$.rmTerms()$terms
+            bsTerms <- private$.bsTerms()
+            
+            summaryResult <- summary(result)
+            model <- summaryResult$univariate.tests
+            epsilon <- summaryResult$pval.adjustments
+            mauchly <- summaryResult$sphericity.tests
+            
+            rmRowKeys <- rmTable$rowKeys
+            bsRowKeys <- bsTable$rowKeys
+            
+            resultRows <- jmvcore::decomposeTerms(as.list(rownames(model)))
+            
+            for (i in seq_along(resultRows)) {
+                
+                rmIndex <- which(sapply(rmRowKeys, function(x) setequal(resultRows[[i]], x)))
+                bsIndex <- which(sapply(bsRowKeys, function(x) setequal(resultRows[[i]], x)))
+                
+                if (length(rmIndex) > 0 || length(bsIndex) > 0) {
+                    
+                    row <- list()
+                    row[["ss"]] <- model[i,"SS"]
+                    row[["df"]] <- model[i,"num Df"]
+                    row[["ms"]] <- row[["ss"]] / row[["df"]]
+                    row[["F"]] <- model[i,"F"]
+                    row[["p"]] <- model[i,"Pr(>F)"]
+                    
+                    # SSr <- model[i,"Error SS"]
+                    # # SSt <- sum(model[indices,"SS"]) + SSr
+                    # SSt <- 10
+                    # MSr <- SSr/model[i,"den Df"]
+                    # 
+                    # row[["eta"]] <- row[["ss"]] / SSt
+                    # row[["partEta"]] <- row[["ss"]] / (row[["ss"]] + SSr)
+                    # row[["omega"]] <- (row[["ss"]] - (row[["df"]] * MSr)) / (SSt + MSr)
+                    
+                    if (length(rmIndex) > 0) {
+                        rmTable$setRow(rowNo=rmIndex, values=row)
+                    } else {
+                        bsTable$setRow(rowNo=bsIndex, values=row)
+                    }
+                }
+            }
+            
+            resRowKeys <- rmRowKeys[which(sapply(rmRowKeys, function(x) ".RES" %in% x))]
+            
+            for (i in seq_along(resRowKeys)) {
+                
+                resid <- resRowKeys[[i]][ ! resRowKeys[[i]] %in% ".RES"]
+                index <- which(sapply(as.list(resultRows), function(x) setequal(x, resid)))
+                
+                row <- list()
+                row[["ss"]] <- model[index,"Error SS"]
+                row[["df"]] <- model[index,"den Df"]
+                row[["ms"]] <- row[["ss"]] / row[["df"]]
+                row[["F"]] <- ""
+                row[["p"]] <- ""
+                
+                rmTable$setRow(rowKey=resRowKeys[[i]], values=row)
+            }
+            
+            row <- list()
+            row[["ss"]] <- model["(Intercept)","Error SS"]
+            row[["df"]] <- model["(Intercept)","den Df"]
+            row[["ms"]] <- row[["ss"]] / row[["df"]]
+            row[["F"]] <- ""
+            row[["p"]] <- ""
+            
+            bsTable$setRow(rowKey="Residual", values=row)
         })
 )
 
