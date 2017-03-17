8ef89eecb59745f124888a44ce3cb1b7c10deaa3
raviselker <selker.ravi@gmail.com>
2017-03-17 13:04:06 +0100

    Added/modified tests
    - Correlation
    - Reliability
    - T-tests

--- tests/testthat/testttestis.R
+++ tests/testthat/testttestis.R
@@ -7,7 +7,9 @@ test_that('ttestIS works', {
     z <- c(2,NA,NaN,3,-1,-2,1,1,-2,2,-2,-3,3)
 
     data <- data.frame(x=x,y=y,z=z)
-    ttest <- ttestIS(data, c("y","z"), "x", hypothesis="different", welch=TRUE, mann=TRUE, norm=TRUE, eqv=TRUE, meanDiff=TRUE, effectSize=TRUE, ci=TRUE, desc=TRUE)
+    ttest <- ttestIS(data, c("y","z"), "x", hypothesis="different", bf=TRUE, welch=TRUE, mann=TRUE, norm=TRUE, eqv=TRUE, meanDiff=TRUE, effectSize=TRUE, ci=TRUE, desc=TRUE)
+
+    expect_equal(c("a"=0.167541563316678), ttest$ttest$getCell(rowNo=2, "es[stud]")$value)
 
     expect_error(ttestIS(data.frame(badGroupingVar=as.factor(c("a", "b", "c")),y=c(1,7,4)), "y", "badGroupingVar"), "Grouping variable 'badGroupingVar' must have exactly 2 levels", fixed=TRUE)
     expect_error(ttestIS(data, "y", "x", hypothesis="error"), "Argument 'hypothesis' must be one of 'different', 'oneGreater', 'twoGreater'", fixed=TRUE)
--- tests/testthat/testttestps.R
+++ tests/testthat/testttestps.R
@@ -9,6 +9,16 @@ test_that('ttestPS works', {
     w <- c(0,4,19,5,9,15,1,4,19,10,13,7,5)
 
     data <- data.frame(x=x,y=y,z=z,w=w)
-    ttest <- ttestPS(data, c("y","z","y","w"), hypothesis="different", wilcoxon=TRUE, meanDiff=TRUE, effectSize=TRUE, ci=TRUE, desc=TRUE)
+    pairs <- list(
+                list(i1='z', i2='w'))
 
+    ttest <- ttestPS(data, pairs, hypothesis="different", bf=TRUE, wilcoxon=TRUE, meanDiff=TRUE, effectSize=TRUE, ci=TRUE, desc=TRUE)
+
+    # Main table
+    expect_equal(-1.03769723962266, ttest$ttest$getCell(rowNo=1, "es[stud]")$value)
+    expect_equal(8.78867448064374, ttest$ttest$getCell(rowNo=1, "stat[bf]")$value)
+
+    # Descriptives table
+    expect_equal(1, ttest$desc$getCell(rowNo=1, "med")$value)
+    expect_equal(5.89915248150105, ttest$desc$getCell(rowNo=2, "sd")$value)
 })
