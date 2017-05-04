407bf521fefb8585f02e557463e87442fcadaa64
raviselker <selker.ravi@gmail.com>
2017-05-04 14:44:22 +0200

    Modified tests for update psych package

--- tests/testthat/testefa.R
+++ tests/testthat/testefa.R
@@ -17,12 +17,13 @@ test_that('efa works', {
     attr(data, 'row.names') <- seq_len(length(data[[1]]))
     attr(data, 'class') <- 'data.frame'
 
-    efa <- jmv::efa(data = data, vars = c("y1","y2","y3 plus","z1","z2"), hideLoadings = .3, rotation = 'varimax')
+    efa <- jmv::efa(data = data, vars = c("y1","y2","y3 plus","z1","z2"),
+                    nFactorMethod = 'fixed', nFactors = 2, hideLoadings = .3, rotation = 'varimax')
 
     # Test loadings table
     expect_equal("", efa$loadings$getCell(rowNo=2, "pc2")$value)
-    expect_equal(0.9696016930218, efa$loadings$getCell(rowNo=2, "pc1")$value)
-    expect_equal(0.0590209944119937, efa$loadings$getCell(rowNo=4, "uniq")$value)
+    expect_equal(0.969985600224343, efa$loadings$getCell(rowNo=2, "pc1")$value)
+    expect_equal(0.00443466058941522, efa$loadings$getCell(rowNo=4, "uniq")$value)
     expect_equal("y3 plus", efa$loadings$getCell(rowNo=3, "name")$value)
 
     expect_error(jmv::efa(data = data, vars = c("y1","y2","y3 plus","z1","z2"), nFactorMethod = "fixed", nFactors = 6),
--- tests/testthat/testreliability.R
+++ tests/testthat/testreliability.R
@@ -2,17 +2,20 @@ context('reliability')
 
 test_that('reliability works', {
 
-    x <- c(4,4,3,4,8,0,9,8,8,6,0,3)
-    y <- c(4,4,3,4,8,0,9,8,8,6,0,3)
-    z <- c(NA,NaN,3,-1,-2,1,1,-2,2,-2,-3,3)
-    inf <- c(Inf, Inf, -Inf, 2, 4, 2, 1.2, 3, 4, 2.3, 5.3, 2.23)
+    data <- list()
+    data[["y1"]] <- c(4,4,3,4,8,0,9,8,8,6,0,3)
+    data[["y2"]] <- c(4,4,3,4,8,0,9,8,8,6,0,3)
+    data[["y3 plus"]] <- c(NA,NaN,3,-1,-2,1,1,-2,2,-2,-3,3)
+    data[["inf"]] <- c(Inf, Inf, -Inf, 2, 4, 2, 1.2, 3, 4, 2.3, 5.3, 2.23)
 
-    data <- data.frame(x=x, y=y, z=z, inf=inf)
-    rel <- jmv::reliability(data, c("x", "y", "z"), omegaScale = TRUE, meanScale = TRUE, alphaItems = TRUE, omegaItems = TRUE)
+    attr(data, 'row.names') <- seq_len(length(data[[1]]))
+    attr(data, 'class') <- 'data.frame'
+
+    rel <- jmv::reliability(data, c("y1", "y2", "y3 plus"), omegaScale = TRUE, meanScale = TRUE, alphaItems = TRUE, omegaItems = TRUE)
 
     expect_equal(0.636883, rel$scale$getCell(rowNo=1, "alpha")$value)
-    expect_equal(6.59471190861493e-08, rel$items$getCell(rowNo=1, "omega")$value)
-    expect_error(jmv::reliability(data, c("x", "inf")), "Item 'inf' contains infinite values", fixed=TRUE)
-    expect_true(is.null(jmv::reliability(data, c("x"))$scale$getCell(rowNo=1, "alpha")$value))
+    expect_equal(0, rel$items$getCell(rowNo=1, "omega")$value)
+    expect_error(jmv::reliability(data, c("y1", "inf")), "Item 'inf' contains infinite values", fixed=TRUE)
+    expect_true(is.null(jmv::reliability(data, c("y1"))$scale$getCell(rowNo=1, "alpha")$value))
 })
 
