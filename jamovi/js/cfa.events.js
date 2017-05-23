3a9bfbe8e23a2608b434ef97d02b05b32f167e4b
raviselker <selker.ravi@gmail.com>
2017-05-23 10:16:21 +0200

    CFA: first commit
    CFA: added lavaan suggest
    
    CFA: moved lavaan to dependencies instead of suggests
    
    CCA: some tweaks
    
    Added chi-square test
    Fixed mod. indices bug
    Added highlighting for model performance
    
    CFA: tweaks
    
    - Fine tuned the UI
    - Added table descriptions
    - Added example
    
    CFA: improved ready state
    
    incorporated Damo's UI fixes
    
    Added a dot in front of the property "setter"
    
    Damo's fix of UI behavior

---
+++ jamovi/js/cfa.events.js
@@ -0,0 +1,68 @@
+
+const events = {
+
+    update: function(ui) {
+        updateModelLabels(ui);
+        calcModelTerms(ui, this);
+    },
+
+    onEvent_test_listItemsAdded: function(ui, data) {
+        updateModelLabels(ui);
+        calcModelTerms(ui, this);
+        setTimeout(() => {
+            data.item.controls[0].$input.focus();
+        }, 0);
+    },
+
+    onEvent_test_listItemsChanged: function(ui) {
+        updateModelLabels(ui);
+        calcModelTerms(ui, this);
+    },
+
+    onChange_resCovSupplier: function(ui) {
+        let values = this.itemsToValues(ui.resCovSupplier.value());
+        this.checkValue(ui.resCov, true, values, FormatDef.term);
+    },
+
+    onEvent_factorNameChange : function(ui) {
+        updateModelLabels(ui);
+    }
+};
+
+const updateModelLabels = function(ui) {
+    let list = ui.factors.applyToItems(0, (item, index) => {
+        let value = item.controls[0].value();
+        if ( ! value || value.trim() === '')
+            item.controls[0].setValue("Factor " + (index + 1) );
+    });
+};
+
+const calcModelTerms = function(ui, context) {
+
+    let factorList = context.cloneArray(ui.factors.value(), []);
+
+    let variables = [];
+    for (let i = 0 ; i < factorList.length; i++) {
+        let vars = factorList[i].vars;
+        if (vars) {
+            for (let y = 0; y < vars.length; y++) {
+                let variable = vars[y];
+                if (variable) {
+                    let found = false;
+                    for (let j = 0; j < variables.length; j++) {
+                        if (variables[j] === variable) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found == false)
+                        variables.push(variable);
+                }
+            }
+        }
+    }
+
+    ui.resCovSupplier.setValue(context.valuesToItems(variables, FormatDef.variable));
+};
+
+module.exports = events;
