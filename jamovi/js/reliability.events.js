f6b6823b877fa856801291f8c27879996c008381
dropmann <damian_dropmann@me.com>
2017-05-26 09:54:21 +1000

    Fixed variables not being removed from 'Reverse Scaled Items' box
    - fixes jamovi/jamovi#241

--- jamovi/js/reliability.events.js
+++ jamovi/js/reliability.events.js
@@ -2,6 +2,11 @@
 const events = {
     onChange_vars: function(ui) {
         ui.revItemsSupplier.setValue(this.valuesToItems(ui.vars.value(), FormatDef.variable));
+    },
+
+    onChange_revItemsSupplier: function(ui) {
+        let values = this.itemsToValues(ui.revItemsSupplier.value());
+        this.checkValue(ui.revItems, true, values, FormatDef.variable);
     }
 };
 
