4609f9d1701aabfbe65cffd58fa7209faefbed04
raviselker <selker.ravi@gmail.com>
2017-08-29 15:11:51 +0200

    Descriptives: fixes bug histogram
    Levels were reverted, but not in the right way.

--- R/descriptives.b.R
+++ R/descriptives.b.R
@@ -572,7 +572,7 @@ descriptivesClass <- R6::R6Class(
 
             } else {
 
-                levels(data$s1) <- rev(levels(data$s1))
+                data$s1 <- factor(data$s1, rev(levels(data$s1)))
 
                 plot <- ggplot(data=data, aes_string(x=names$x, y=names$s1, fill=names$s1)) +
                     labs(list(x=labels$x, y=labels$s1)) +
