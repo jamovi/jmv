401860ef46e5e9d00ce6876f98fbd509802ae1f9
Jonathon Love <jon@thon.cc>
2019-07-20 15:17:45 +1000

    Removed deprecated anova

--- R/deprecated.R
+++
@@ -1,8 +0,0 @@
-
-#' @rdname ANOVA
-#' @param ... arguments passed to anova() are passed on to ANOVA()
-#' @export
-anova <- function(...) {
-    .Deprecated('ANOVA', 'anova() from jmv is deprecated, use ANOVA() instead')
-    ANOVA(...)
-}
