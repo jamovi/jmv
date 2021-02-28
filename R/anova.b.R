20f00e51181b5995cd74b89920e1a44f6c62d2cc
Jonathon Love <jon@thon.cc>
2021-02-28 15:24:02 +1100

    AN(C)OVA: Added outputs

--- R/anova.b.R
+++ R/anova.b.R
@@ -11,6 +11,9 @@ anovaClass <- R6::R6Class(
                 self$results$main$setTitle(paste0('ANOVA - ', self$options$dep))
             else
                 self$results$main$setTitle('ANOVA - \u2026')
+
+            self$results$resids$setDescription('Residuals from ANOVA')
+            self$results$predict$setDescription('Predicted values from ANOVA')
         }
     ),
     public = list(
