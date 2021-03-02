4b2e4d35dd12760141c34dc5bf23c2e8ad12188b
Ravi <selker.ravi@gmail.com>
2021-03-02 21:52:37 +0100

    AN(C)OVA: change naming output variables
    * removed predicted values output variable

--- R/anova.b.R
+++ R/anova.b.R
@@ -12,8 +12,7 @@ anovaClass <- R6::R6Class(
             else
                 self$results$main$setTitle('ANOVA - \u2026')
 
-            self$results$resids$setDescription('Residuals from ANOVA')
-            self$results$predict$setDescription('Predicted values from ANOVA')
+            self$results$residsOV$setDescription('Residuals from ANOVA')
         }
     ),
     public = list(
