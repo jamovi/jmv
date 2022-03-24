1ca82010e54cad8aa9f46a91098584a9411fdc90
Jonathon Love <jon@thon.cc>
2022-03-24 18:08:22 +1100

    Clean up of data files
    Some R policy changed here?

--- R/00meta.R
+++ R/00meta.R
@@ -1,6 +1,31 @@
-#' Bugs (Ryan, Wilde & Crist, 2013)
+
+#' bugs
 #'
 #' @name bugs
+#' @docType data
+#' @author Ryan, Wilde & Crist (2013)
+#'
+#' @references
+#' \url{http://faculty.kutztown.edu/rryan/RESEARCH/PUBS/Ryan,\%20Wilde,\%20\%26\%20Crist\%202013\%20Web\%20exp\%20vs\%20lab.pdf}
+NULL
+
+#' Tooth Growth
+#'
+#' @name ToothGrowth
+#' @docType data
+#' @keywords data
+NULL
+
+#' Big 5
+#'
+#' @name Big5
+#' @docType data
+#' @keywords data
+NULL
+
+#' iris
+#'
+#' @name iris
 #' @docType data
 #' @keywords data
 NULL
--- R/data.R
+++ R/data.R
@@ -1,10 +1,1 @@
 
-#' data sets
-#'
-#' @name bugs
-#' @docType data
-#' @author Ryan, Wilde & Crist (2013)
-#'
-#' @references
-#' \url{http://faculty.kutztown.edu/rryan/RESEARCH/PUBS/Ryan,\%20Wilde,\%20\%26\%20Crist\%202013\%20Web\%20exp\%20vs\%20lab.pdf}
-NULL
