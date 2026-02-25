5a04cc1dc26a4c99174f67487461a3697db67b60
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2026-02-25 11:59:37 +0900

    Moved calculation of Durbin-Conover-Test into a separate function (calcDurbin), added option for p-adjustment, and added further unit tests

--- R/anovarmnp.b.R
+++ R/anovarmnp.b.R
@@ -32,34 +32,15 @@ anovaRMNPClass <- R6::R6Class(
 
             table  <- self$results$get('comp')
 
-            # adapted from https://www.nist.gov/ -> search "Friedman Test" and
-            # https://en.wikipedia.org/wiki/Durbin_test
-            # NB: assumes that rows with NAs are omitted
-            # b denotes blocks (participant / unit of observation)
-            b <- nrow(mat)
-            # k denotes treatments (conditions / measures)
-            k <- ncol(mat)
-
-            # assign ranks per block and calculate the rank sum per treatment 
-            Rij <- apply(mat, 1, rank)
-            Rj <- apply(Rij, 1, sum)
-
-            # use the formulas on the NIST web page to calculate the t-value
-            # for the rank sum, which is then used to determine the p-value
-            A1 <- sum(Rij ^ 2)
-            C1 <- (b * k * (k + 1) ^ 2) / 4
-            T1 <- (k - 1) / (A1 - C1) * (sum(Rj ^ 2) - b * C1)
-            DF <- (b - 1) * (k - 1)
-            D <- sqrt(((2 *  (A1 - C1) * b) / DF) * (1 - T1 / (b * (k - 1))))
-
-            rowNo <- 1
-            for (i in 1:(k - 1)) {
-                for (j in (i + 1):k) {
-                    T <- abs(Rj[i] - Rj[j]) / D
-                    table$setRow(rowNo = rowNo, list(stat = T, p = 2 * (1 - pt(T, DF))))
-                    rowNo <- rowNo + 1
+            resDurbin <- calcDurbin(mat)
+            
+            for (i in seq_len(nrow(resDurbin))) {
+                if (table$getCell("i1", rowNo = i)$value == resDurbin[i, "i1"] &&
+                    table$getCell("i2", rowNo = i)$value == resDurbin[i, "i2"]) {
+                    table$setRow(rowNo = i, list(stat = resDurbin[i, "t"], p = resDurbin[i, "p"]))
                 }
             }
+
         }
 
         private$.preparePlot(data)
@@ -131,3 +112,33 @@ anovaRMNPClass <- R6::R6Class(
         return(p)
     })
 )
+
+calcDurbin=function(mat, adjustP = "none") {
+    adjustP <- match.arg(adjustP, p.adjust.methods)
+
+    # adapted from https://www.nist.gov/ -> search "Friedman Test" and
+    # https://en.wikipedia.org/wiki/Durbin_test
+    # NB: assumes that rows with NAs are omitted
+    # b denotes blocks (participant / unit of observation)
+    b <- nrow(mat)
+    # k denotes treatments (conditions / measures)
+    k <- ncol(mat)
+
+    # assign ranks per block and calculate the rank sum per treatment 
+    Rij <- apply(mat, 1, rank)
+    Rj  <- apply(Rij, 1, sum)
+
+    # use the formulas on the NIST web page to calculate the t-value
+    # for the rank sum, which is then used to determine the p-value
+    A1 <- sum(Rij ^ 2)
+    C1 <- (b * k * (k + 1) ^ 2) / 4
+    T1 <- (k - 1) / (A1 - C1) * (sum(Rj ^ 2) - b * C1)
+    DF <- (b - 1) * (k - 1)
+    DN <- sqrt(((2 *  (A1 - C1) * b) / DF) * (1 - T1 / (b * (k - 1))))
+
+    combPairs <- t(combn(colnames(mat), 2))
+    valT <- apply(combPairs, 1, function(c) abs(diff(Rj[c])) / DN)
+
+    data.frame(i1 = combPairs[, 1], i2 = combPairs[, 2],
+               t = valT, p = stats::p.adjust(2 * (1 - pt(valT, DF)), adjustP))
+}
